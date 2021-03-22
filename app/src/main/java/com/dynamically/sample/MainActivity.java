40d7f8a8cf032007ae4eac067255eea433c46bb9
fynjy7012 <77893412+fynjy7012@users.noreply.github.com>
2021-03-22 22:13:49 +0300

    Типо заработало

--- app/src/main/java/com/dynamically/sample/MainActivity.java
+++ app/src/main/java/com/dynamically/sample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.prodrive;
+package com.dynamically.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class ListActivity extends Activity {
+public class MainActivity extends Activity {
 
     //Создаем список вьюх которые будут создаваться
     private List<View> allEds;
@@ -38,32 +38,40 @@ public class ListActivity extends Activity {
                 //берем наш кастомный лейаут находим через него все наши кнопки и едит тексты, задаем нужные данные
                 final View view = getLayoutInflater().inflate(R.layout.custom_edittext_layout, null);
                 Button deleteField = (Button) view.findViewById(R.id.button2);
+                deleteField.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            ((LinearLayout) view.getParent()).removeView(view);
+                            allEds.remove(view);
+                        } catch(IndexOutOfBoundsException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });
                 EditText text = (EditText) view.findViewById(R.id.editText);
-                text.setText("Новая задача" + counter);
+                text.setText("Some text" + counter);
                 //добавляем все что создаем в массив
                 allEds.add(view);
                 //добавляем елементы в linearlayout
                 linear.addView(view);
-
-
-                Button showDataBtn = (Button) findViewById(R.id.button3);
-                showDataBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //преобразуем наш ArrayList в просто String Array
-                        String[] items = new String[allEds.size()];
-                        //запускаем чтение всех елементов этого списка и запись в массив
-                        for (int i = 0; i < allEds.size(); i++) {
-                            items[i] = ((EditText) allEds.get(i).findViewById(R.id.editText)).getText().toString();
-
-                            //ну и можно сразу же здесь вывести
-                            Log.e("", ((EditText) allEds.get(i).findViewById(R.id.editText)).getText().toString());
-                        }
-                    }
-                });
             }
+        });
 
+        Button showDataBtn = (Button) findViewById(R.id.button3);
+        showDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //преобразуем наш ArrayList в просто String Array
+                String [] items = new String[allEds.size()];
+                //запускаем чтение всех елементов этого списка и запись в массив
+                for(int i=0; i < allEds.size(); i++) {
+                    items[i] = ((EditText) allEds.get(i).findViewById(R.id.editText)).getText().toString();
 
+                    //ну и можно сразу же здесь вывести
+                    Log.e("", ((EditText) allEds.get(i).findViewById(R.id.editText)).getText().toString());
+                }
+            }
         });
     }
 }
--- app/src/main/java/com/example/prodrive/MainActivity.java
+++ app/src/main/java/com/example/prodrive/MainActivity.java
@@ -1,43 +1,81 @@
 package com.example.prodrive;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.dynamically.sample.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+
+    //Создаем список вьюх которые будут создаваться
+    private List<View> allEds;
+    //счетчик чисто декоративный для визуального отображения edittext'ov
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        Button addButton = (Button) findViewById(R.id.button);
+        //инициализировали наш массив с edittext.aьи
+        allEds = new ArrayList<View>();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        //находим наш linear который у нас под кнопкой add edittext в activity_main.xml
+        final LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // получим идентификатор выбранного пункта меню
-        int id = item.getItemId();
+                //берем наш кастомный лейаут находим через него все наши кнопки и едит тексты, задаем нужные данные
+                final View view = getLayoutInflater().inflate(R.layout.custom_edittext_layout, null);
+                Button deleteField = (Button) view.findViewById(R.id.button2);
+                deleteField.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            ((LinearLayout) view.getParent()).removeView(view);
+                            allEds.remove(view);
+                        } catch(IndexOutOfBoundsException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });
+                EditText text = (EditText) view.findViewById(R.id.editText);
+                text.setText("Some text" + counter);
+                //добавляем все что создаем в массив
+                allEds.add(view);
+                //добавляем елементы в linearlayout
+                linear.addView(view);
+            }
+        });
 
-        TextView infoTextView = findViewById(R.id.textView);
+        Button showDataBtn = (Button) findViewById(R.id.button3);
+        showDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //преобразуем наш ArrayList в просто String Array
+                String [] items = new String[allEds.size()];
+                //запускаем чтение всех елементов этого списка и запись в массив
+                for(int i=0; i < allEds.size(); i++) {
+                    items[i] = ((EditText) allEds.get(i).findViewById(R.id.editText)).getText().toString();
 
-        // Операции для выбранного пункта меню
-        switch (id) {
-            case R.id.action_settings:
-                infoTextView.setText("Вы выбрали кота!");
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+                    //ну и можно сразу же здесь вывести
+                    Log.e("", ((EditText) allEds.get(i).findViewById(R.id.editText)).getText().toString());
+                }
+            }
+        });
     }
 }
 
